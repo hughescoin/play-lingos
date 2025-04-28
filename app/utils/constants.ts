8644147e676ffd91022f1f23a5f17b28d1f460e6
Patrick Hughes <patrick.hughes@coinbase.com>
2025-04-27 22:19:12 -0500

    update env var

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -3,4 +3,4 @@ export const GUESS_GAME_ADDRESS = process.env
 export const PAYMASTER_URL = process.env.NEXT_PUBLIC_PAYMASTER_PROXY_SERVER_URL;
 export const PLAY_FEE = '0.00005';
 export const GUESS_GAME_SCORING_ADDRESS = process.env
-  .NEXT_PUBLIC_GUESS_GAME_SCORING_ADDRESS as `0x${string}`;
+  .NEXT_PUBLIC_GUESS_GAME_SCORE_ADDRESS as `0x${string}`;
